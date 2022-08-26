cd3336da320ce775ac996fb689be990b4b68ae72
SUDARSAN PERUMAL V <sudarsanperumalv@SUDARSANs-MacBook-Air.local>
2022-08-26 15:56:36 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './App.css'
+const App = () =>{
+    return (
+        <h1>
+            Hello world! I am using React
+        </h1>
+    )
+}
+
+export default App
